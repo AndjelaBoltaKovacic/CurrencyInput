fd32a991b4c6019670fcf3ca240ec10513839e43
Andjela Bolta Kovacic <a.boltakovacic@vegait.rs>
2023-09-17 13:06:14 +0200

    Added tests

---
+++ tests/index.test.tsx
@@ -0,0 +1,63 @@
+import { render, fireEvent, queryByAttribute } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect"
+import CurrencyInput from '../src/index';
+
+describe('CurrencyInput', () => {
+    it('renders without crashing', () => {
+        render(<CurrencyInput onChange={() => { }} type="text" />);
+    });
+
+    it('displays the default value correctly', () => {
+        const { getByDisplayValue } = render(
+            <CurrencyInput onChange={() => { }} defaultValue="1234.56" type="text" />
+        );
+        const inputElement = getByDisplayValue('1,234.56');
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    it('calls the onChange function with the correct value when input changes', () => {
+        const mockOnChange = jest.fn();
+        const component = render(
+            <CurrencyInput onChange={mockOnChange} type="text" />
+        );
+        const getById = queryByAttribute.bind(null, 'id');
+        const inputElement = getById(component.container, 'currency-input'); // Use getById to select by id attribute
+        expect(inputElement).toBeInTheDocument();
+
+        inputElement && fireEvent.change(inputElement, { target: { value: '1234.56' } });
+
+        expect(mockOnChange).toHaveBeenCalledWith('1,234.56');
+    });
+
+    fit('formats the input value correctly with custom styles', () => {
+        const component = render(
+            <CurrencyInput
+                onChange={() => { }}
+                defaultValue="789.12"
+                type="text"
+                customStyles={{ color: 'red', fontSize: '16px' }}
+            />
+        );
+        const getById = queryByAttribute.bind(null, 'class');
+        const inputContainer = getById(component.container, 'inputContainer');
+        expect(inputContainer).toBeInTheDocument();
+        expect(inputContainer).toHaveStyle('color: red');
+        expect(inputContainer).toHaveStyle('font-size: 16px');
+    });
+
+    it('renders the currency symbol when currencyCode is provided', () => {
+        const { getByText } = render(
+            <CurrencyInput
+                onChange={() => { }}
+                defaultValue="1234.56"
+                type="text"
+                currencyCode="USD"
+            />
+        );
+        const currencySymbolElement = getByText('$');
+
+        expect(currencySymbolElement).toBeInTheDocument();
+    });
+
+    // Add more test cases as needed
+});
